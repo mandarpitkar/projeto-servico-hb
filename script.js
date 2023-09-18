57d53efb7a2972d144d757a80fae3bd4520b19b9
mandarpitkar <139145085+mandarpitkar@users.noreply.github.com>
2023-09-18 15:16:47 -0300

    atualizei o site

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function inittabMenu() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabMenu.length && tabContent.length){
-    tabContent[0].classList.add('ativo')
-function activeTab(index){
-    tabContent.forEach((section)=>{
-        section.classList.remove('ativo');     
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo')
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            })
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
+        });
+    }
+}
+inittabMenu();
+
+
+function initlinkInternos(){
+    const linkInternos = document.querySelectorAll('a[href^="#"]')
+
+    function handleLink(event) {
+        console.log(event);
+    }
+    linkInternos.forEach((link) => {
+        link.addEventListener('click', handleLink);
     })
-    tabContent[index].classList.add('ativo');
 }
+initlinkInternos();
 
-tabMenu.forEach((itemMenu, index)=> {
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    });
-});
-}
-
-const linkInternos = document.querySelectorAll('a[href^="#"]')
-
-function handleLink(event) {
-    console.log(event);
-} 
-linkInternos.forEach((link) => {
-    link.addEventListener('click', handleLink);
-})
 
 // function clickMenu() {
 //     if(menu.style.display == 'block'){
